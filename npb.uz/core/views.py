10fb02f87cdb3c346b25260ea9b4d854e1ad2242
Bekzodergashev <81473754+bekzodergashev1@users.noreply.github.com>
2022-02-08 16:15:20 +0500

    .

--- npb.uz/core/views.py
+++ npb.uz/core/views.py
@@ -1,6 +1,18 @@
-from django.shortcuts import render
-
+from django.shortcuts import redirect, render
+from .forms import ContactForm
+from requests import get
 # Create your views here.
 
 def home(request):
+    print(request.method)
+    if request.method == "POST":
+        print(request.POST)
+        form = ContactForm(data=request.POST)
+        if form.is_valid():
+            fullname = request.POST.get("fullname")
+            phone = request.POST.get("phone")
+            get("https://api.telegram.org/bot5046439124:AAFUJMJzy2dCWNObY1rbEZIWpJ6P7fpR0nA/sendMessage?chat_id=@nbpplast&text={}".format(
+                f"Имя: {fullname}\nТелефонный номер: {phone}"
+            ))
+            return redirect('/')
     return render(request, 'index.html')
